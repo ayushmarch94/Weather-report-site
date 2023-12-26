c02ea2c402eecfcd8f880dbe748f8d3704d0ae64
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2023-12-26 23:38:32 +0530

    Generated API call

--- main.js
+++ main.js
@@ -10,11 +10,58 @@ const options = {
 async function fetchData() {
     try {
         const response = await fetch(url, options);
-        const result = await response.text();
-        console.log(result);
+        const result = await response.json();
+        return result
     } catch (error) {
         console.error(error);
     }
 }
 
-fetchData(); // Call the async function to start fetching data
+
+async function getData() {
+    try {
+        // Fetch data
+        const data = await fetchData();
+        console.log(data);
+
+        // Access weather details
+        let pct = data.cloud_pct;
+        let feels = data.feels_like;
+        let humidity = data.humidity;
+        let temp = data.temp;
+        let min_temp = data.min_temp;
+        let max_temp = data.max_temp;
+        let wind_speed = data.wind_speed;
+        let wind_degrees = data.wind_degrees;
+        let sunrise = data.sunrise;
+        let sunset = data.sunset;
+
+        let changePct =document.getElementById('changePct');
+        let changeFeel =document.getElementById('changeFeel');
+        let changeHumid =document.getElementById('changeHumid');
+        let changeTemp =document.getElementById('changeTemp');
+        let changeMax =document.getElementById('changeMax');
+        let changeMin =document.getElementById('changeMin');
+        let changeWindSpeed =document.getElementById('changeWindSpeed');
+        let changeWindDegree =document.getElementById('changeWindDegree');
+        let changeSunRise =document.getElementById('changeSunRise');
+        let changeSunSet =document.getElementById('changeSunSet');
+
+
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+        changePct.innerHTML=`Cloud percipitation : ${pct}`;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+getData();
