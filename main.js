d90ab43f9aee71e9f1488d0509c985f9cb455acb
AYUSH KUMAR <125579641+ayushmarch94@users.noreply.github.com>
2024-07-27 20:44:10 +0530

    updated the api key

--- main.js
+++ main.js
@@ -1,68 +1,30 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'a6db9daa17msh107d70f18efdec5p1734b4jsn399a3df2ecf1',   // API key
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'     //API end point
-	}
-};
+const btn = document.getElementById("button1");
+const searchInput = document.getElementById("search");
+const apiKey = "3749334cbea6c45376f910df4e0371b9";
+const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function fetchData(city) {
-    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
-    try {
-        const response = await fetch(url, options);
-        const result = await response.json();
-        return result;
-    } catch (error) {
-        console.error(error);
-    }
-}
+btn.addEventListener("click", () => {
+    const cityName = searchInput.value.trim();
 
 
-async function getData(obj) {
-    try {
-        // Fetch data
-        const data = await obj
-
-        //Access html content
-        let Pct =document.getElementById('Pct');
-        let Feel =document.getElementById('Feel');
-        let Humid =document.getElementById('Humid');
-        let Temp =document.getElementById('Temp');
-        let Max =document.getElementById('Max');
-        let Min =document.getElementById('Min');
-        let WindSpeed =document.getElementById('WindSpeed');
-        let WindDegree =document.getElementById('WindDegree');
-        let SunRise =document.getElementById('SunRise');
-        let SunSet =document.getElementById('SunSet');
-
-        //Displayed data
-        Pct.innerHTML=`Cloud precipitation : ${data.cloud_pct}`;
-        Feel.innerHTML=`Feels like : ${data.feels_like}°C`;
-        Humid.innerHTML=`Humidity : ${data.humidity}%`;
-        Temp.innerHTML=` ${data.temp}°C`;
-        Max.innerHTML=`Maximum Temperature : ${data.max_temp}°C`;
-        Min.innerHTML=`Minimum Temperature : ${data.min_temp}°C`;
-        WindSpeed.innerHTML=`Wind Speed : ${data.wind_speed}Km/h`;
-        WindDegree.innerHTML=`Wind Degree : ${data.wind_degrees}`;
-        SunRise.innerHTML=`Sunrise time : ${data.sunrise}`;
-        SunSet.innerHTML=`Sunset time : ${data.sunset}`;
-
-
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-let button=document.getElementById('button1');
-let inputCity=document.getElementById('search')
-button.addEventListener('click', async ()=>{
-    var city=inputCity.value.trim();        // city name from input field
-    console.log(city);
-    var obj=fetchData(city);    // we fetch data from api for specific city
-    getData(obj);               // HTML content manipulation
-})
-
-
-
+    fetch(url + cityName + `&appid=${apiKey}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data);  
+            document.getElementById("Feel").innerHTML = "Feel like : " + data.main.feels_like +" deg";
+            document.getElementById("Humid").innerHTML ="Humidity : " + data.main.humidity + " %";
+            document.getElementById("Temp").innerHTML ="Current Temperature " + data.main.temp + " deg";
+            document.getElementById("Max").innerHTML ="Maximum Temperature : "+ data.main.temp_max + " deg";
+            document.getElementById("Min").innerHTML ="Minimum Temperature : " + data.main.temp_min + " deg";
+            document.getElementById("WindSpeed").innerHTML ="Wind Speed : " + data.wind.speed + " km/h";
+            document.getElementById("WindDegree").innerHTML ="Wind Degree : " + data.wind.deg + " deg";
+            document.getElementById("SunRise").innerHTML ="Sunrise time : " + data.sys.sunrise.toString().substring(0, 2) + " Hr";
+            document.getElementById("SunSet").innerHTML ="Sunset time : " + data.sys.sunset.toString().substring(0, 2) + " Hr";
+            console.log(data);
+        })
+        .catch(() => {
+            console.log("Data not found or API request failed.");
+        });
+});
 
 
